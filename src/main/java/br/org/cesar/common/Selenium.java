fe81d9fc79670aa6148db3caa61e4939226ae5bb
zecarrera <zecarrera@gmail.com>
2014-03-17 22:28:04 -0300

    Updated project to selenium 2.40.0

--- src/main/java/br/org/cesar/common/Selenium.java
+++ src/main/java/br/org/cesar/common/Selenium.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 
